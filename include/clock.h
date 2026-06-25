9bd2534277342923705fad2b4d422f7a1ff3a67a
lfariazzz <levi.farias@aluno.ufca.edu.br>
2026-06-25 20:33:35 -0300

    (feat)-[prereq] adiciona assinaturas públicas de todos os módulos

---
+++ include/clock.h
@@ -0,0 +1,13 @@
+#ifndef CLOCK_H
+#define CLOCK_H
+
+#include "types.h"
+
+void  clock_init(SimClock *clk);
+void  clock_destroy(SimClock *clk);
+void *clock_thread(void *arg);
+
+long  clock_get_tick(SimClock *clk);
+void  clock_wait_tick(SimClock *clk, long last_tick);
+
+#endif
