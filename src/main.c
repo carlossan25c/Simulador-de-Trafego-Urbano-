6500e97273459942c2ade657fb40388e116253c4
David J. <david.vital@aluno.ufca.edu.br>
2026-06-29 13:02:52 -0300

    feat: adiciona handler de SIGINT com broadcast no clock

--- src/main.c
+++ src/main.c
@@ -20,3 +20,16 @@ static TrafficLight g_lights[NUM_INTERSECTIONS];
 static pthread_t g_clock_tid;
 static pthread_t g_light_tids[NUM_INTERSECTIONS];
 static pthread_t g_vehicle_tids[NUM_VEHICLES];
+
+static volatile sig_atomic_t g_running = 1;
+
+static void handle_sigint(int sig)
+{
+    (void)sig;
+    g_running = 0;
+
+    /* Acorda threads que estão bloqueadas esperando o próximo tick */
+    pthread_mutex_lock(&g_clock.mutex);
+    pthread_cond_broadcast(&g_clock.cond);
+    pthread_mutex_unlock(&g_clock.mutex);
+}
